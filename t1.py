f9a1f8f1e3c2a9f84fa032f2746d487c86127015
OfficialCaptain <hakantheamazing@gmail.com>
2020-09-02 11:14:00 -0600

    added another turtle

--- t1.py
+++ t1.py
@@ -7,16 +7,28 @@ for i in range(int(input("How many colors?: "))):
     colors.append('#%06X' % randint(0, 0xFFFFFF))
 
 
+
+
 wn = turtle.Screen()
+wn.bgcolor("black")
 lorelei = turtle.Turtle()
 length = 1
+hakan = turtle.Turtle()
 
 for i in range(70):
     color = random.choice(colors)
     lorelei.forward(length)
     lorelei.right(90)
     lorelei.color(color)
-    length = length + 10
+    length = length + 5
     lorelei.width(2)
+    lorelei.speed(200)
+    hakan.forward(length)
+    hakan.right(360)
+    hakan.left(90)
+    hakan.color(color)
+    length = length + 5
+    hakan.width(2)
+    hakan.speed(100)
 
 wn.exitonclick()
